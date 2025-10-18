8220cb3980e7179791cad0d61491e7426d2540aa
Homeacces01 <homeaccesssec24@gmail.com>
2025-10-18 10:29:43 -0500

    Cambios en front

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -70,7 +70,7 @@ function Sidebar({ logout }) {
   const secondaryBtn = "bg-gray-100 text-gray-900";
 
   return (
-    <div className="flex flex-col h-screen w-64 bg-blue-900 border-r border-blue-800 text-white">
+    <div className="flex flex-col h-screen w-64 bg-blue-900 text-white fixed left-0 top-0 bottom-0 border-r border-blue-800">
       {/* Header */}
       <div className="flex h-16 items-center gap-2 border-b border-sidebar-border px-4">
         <img src={logo} alt="HOME-ACCES" className="h-8 w-8 object-contain" />
